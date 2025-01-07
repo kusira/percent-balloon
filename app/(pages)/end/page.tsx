80220995fc96ea2027f0dc40300cb4f7e43e25a7
chrom9103 <kdkd21347.tm@gmail.com>
2025-01-07 20:04:46 +0900

    headerIntoComp

--- app/(pages)/end/page.tsx
+++ app/(pages)/end/page.tsx
@@ -16,11 +16,12 @@ export default function Home() {
 
           {/* game-end-link */}
           <a
-            href="/hoge"
+            href="./"
             className="inline-block bg-blue-500 text-white py-2 px-6 rounded-lg hover:bg-blue-600"
           >
           Exit
           </a>
+
         </main>
 
         {/* フッター */}
