6003968fb7c71e6b74a11c18cf5874134e7f8f80
Belphegor274 <yuukawamo@gmail.com>
2025-01-09 18:59:34 +0900

    エンドのヘッダー、フッターを修正した

--- app/(pages)/end/page.tsx
+++ app/(pages)/end/page.tsx
@@ -1,3 +1,4 @@
+import Header from "@/app/components/header";
 import Image from "next/image";
 
 export default function Home() {
@@ -5,9 +6,7 @@ export default function Home() {
     <div>
       <div className="bg-[#e8f2f7] flex flex-col items-center justify-center">
         {/* ヘッダー */}
-        <header className="w-full bg-blue-500 text-white py-4">
-          <h1 className="text-center text-2xl"><b>パーセントバルーン</b></h1>
-        </header>
+        <Header />
 
         {/* メインコンテンツ */}
         <main className="mt-8 px-8 text-center">
@@ -25,12 +24,7 @@ export default function Home() {
         </main>
 
         {/* フッター */}
-        <footer className="mt-12 w-full text-center text-gray-600 border-t">
-          <hr className="border-gray-500"></hr>
-          <p className="py-3">
-            © 2024 PiedPiper-奪還鯖缶. All Rights Reserved.
-          </p>
-        </footer>
+        <Header />
       </div>
     </div>
     
