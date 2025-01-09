136f9959dd198a37573cdec66354ca17a7f7ec08
gakki713 <167425396+gakki713@users.noreply.github.com>
2025-01-09 19:19:37 +0900

    Create retry/page.tsx

---
+++ app/(pages)/retry/page.tsx
@@ -0,0 +1,33 @@
+import Footer from "@/app/components/footer";
+import Header from "@/app/components/header";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="bg-[#e8f2f7] flex flex-col items-center justify-center">
+        {/* ヘッダー */}
+				<Header />
+
+        {/* メインコンテンツ */}
+        <main className="mt-8 px-8 text-center">
+          {/* msg */}
+          <h2 className="text-5xl font-bold mb-4">Never give up!!</h2>
+
+          {/* game-end-link */}
+          <a
+            href="./"
+            className="inline-block bg-blue-500 text-white py-2 px-6 rounded-lg hover:bg-blue-600"
+          >
+          Exit
+          </a>
+
+        </main>
+
+        {/* フッター */}
+				<Footer />
+      </div>
+    </div>
+    
+  );
+}
