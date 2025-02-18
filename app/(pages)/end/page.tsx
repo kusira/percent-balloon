64314bd0553ff4b42cabb2749f02485f5de5150d
chrom9103 <kdkd21347.tm@gmail.com>
2025-02-18 19:12:23 +0900

    endとretryのUIを変更

--- app/(pages)/end/page.tsx
+++ app/(pages)/end/page.tsx
@@ -4,30 +4,29 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="bg-[#e8f2f7] flex flex-col items-center justify-center">
+      <div className="bg-[#e8f2f7] flex flex-col min-h-screen">
         {/* ヘッダー */}
         <Header/>
 
         {/* メインコンテンツ */}
-        <main className="mt-8 px-8 text-center">
+        <main className="flex-grow mt-8 px-8 text-center">
           {/* msg */}
           <h2 className="text-5xl font-bold mb-4 text-black">Congratulations!!!</h2>
 
           {/* game-end-link */}
-          <a
+            <a
             href="./"
-            className="inline-block bg-blue-500 text-white py-2 px-6 rounded-lg hover:bg-blue-600"
-          >
-          Exit
-          </a>
-
+            className="inline-block bg-blue-500 text-white py-2 px-8 rounded-lg hover:bg-blue-600 mt-4"
+            >
+            Exit
+            </a>
         </main>
 
         {/* フッター */}
-        <footer className="mt-12 w-full text-center text-gray-600 border-t">
+        <footer className="w-full text-center text-gray-600 border-t">
           <hr className="border-gray-500"></hr>
           <p className="py-3">
-            © 2024 PiedPiper-奪還鯖缶. All Rights Reserved.
+             © 2024 PiedPiper-奪還鯖缶. All Rights Reserved.
           </p>
         </footer>
       </div>
